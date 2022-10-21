0e0c7088f518d9a1c26470a6382b7875d9bdd545
CWilson1993 <cian.wilson10@gmail.com>
2022-10-21 13:21:21 +0000

    updated allowed hosts

--- dream/settings.py
+++ dream/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dreamblog23.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['dreamblog23.herokuapp.com', 'localhost']
 
 
 # Application definition
