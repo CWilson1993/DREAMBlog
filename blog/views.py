d9f2a432f7380b1ecceb26284401c27e9b78c2f0
CWilson1993 <cian.wilson10@gmail.com>
2022-10-27 20:54:41 +0000

    Add admin edit post view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View, CreateView
+from django.views import generic, View, CreateView, UpdateView
 from django.http import HttpResponseRedirect
 from taggit.models import Tag
 from .models import Post
 from .forms import CommentForm
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def handler404(request, exception):
@@ -99,3 +99,18 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
