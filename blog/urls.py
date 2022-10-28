0c33ceabcbae3a69ef14a7135ffbf0472ed0c823
CWilson1993 <cian.wilson10@gmail.com>
2022-10-28 14:07:23 +0000

    Updated styling and Debug=False

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('posts/', views.PostList.as_view(), name='post_list'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('edit/<int:pk>/', views.edit, name='edit'),
     path('post/new/', views.postview, name='post_create'),
     path('delete/<int:pk>/', views.delete, name='delete'),
-    path('edit/<int:pk>/', views.edit, name='edit'),
 ]
